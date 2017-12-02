d7d040dce04c359f023567939bb6096d29c62243
Grant Smith <smith9327@yahoo.com>
2017-12-02 00:35:42 -0600

    initial commit

--- eclipse-workspace/Comp271Lab3/src/TestPerformance.java
+++ eclipse-workspace/Comp271Lab3/src/TestPerformance.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -69,4 +70,5 @@ public class TestPerformance {
       sum += arrayList.get(r % SIZE);
     }
   }
+}
 }
