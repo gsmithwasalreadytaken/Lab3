5af252577cde1d19cb5c32708e58f11014c38b04
Grant Smith <smith9327@yahoo.com>
2017-12-03 22:16:20 -0600

    Intializing variables

--- Desktop/Java/FinalProkject/src/Main.java
+++ Desktop/Java/FinalProkject/src/Main.java
@@ -1,5 +1,31 @@
-public class Main {
+import java.util.ArrayList;
 
-    System.out.println("See if this works");
+public class Main {
+    ArrayList rooms= new ArrayList(6);
+Rooms Dinobili = new Rooms();
+Rooms Damen =new Rooms();
+Rooms Cudahy= new Rooms();
+Rooms SanFran= new Rooms();
+Rooms Mundelein = new Rooms();
+Rooms LSB= new Rooms();
+
+    ArrayList suspects= new ArrayList(6);
+
+Suspects Madonna = new Suspects();
+Suspects Wilbur = new Suspects();
+Suspects Louis = new Suspects();
+Suspects Orville = new Suspects();
+Suspects Dilbert = new Suspects();
+Suspects Ingrid = new Suspects();
+
+    ArrayList weapons= new ArrayList(6);
+
+Weapons SharpPencil = new Weapons();
+Weapons PlasticBag= new Weapons();
+Weapons Shoelace = new Weapons();
+Weapons Flamethrower = new Weapons();
+Weapons Katana = new Weapons();
+Weapons Cannon = new Weapons();
+
 
 }
